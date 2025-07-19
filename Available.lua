4e8609b3480675cbeba9fe7efaa9c8b34260f007
quadshoota <95818124+quadshoota@users.noreply.github.com>
2025-07-19 21:42:19 +0200

    Create Available.lua

---
+++ Available.lua
@@ -0,0 +1,9 @@
+local Available = {
+"arrowdown", "arrowleft", "arrowright", "arrowup",
+"browser", "browsers", "calender", "cloudfile",
+"codeblock", "delete", "download", "downloadcloud",
+"gear", "upload", "uploadtocloud", "archive",
+"checkmark", "copy", "refresh", "user", "date", "version"
+}
+ 
+return Available
