2c3230fc4180867512bdfaa12ba55e37e6d2795a
quadshoota <95818124+quadshoota@users.noreply.github.com>
2025-07-19 22:02:20 +0200

    Create data.lua

---
+++ data.lua
@@ -0,0 +1,17 @@
+
+
+
+local Data =
+{
+    Updated = "2023-07-19 22:00",
+    Version = "1.0.0",
+    UpdatedBy = "kezo",
+    Changelog = {
+        "Added Auto-Collect",
+        "Added Auto-Favourite",
+        "Added Auto-Sell",
+    },
+}
+
+
+return Data
