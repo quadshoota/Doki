2c3cd28d4a9ead56d4858b0c6411deee35b21684
quadshoota <95818124+quadshoota@users.noreply.github.com>
2025-07-21 15:53:26 +0200

    Update Available.lua

--- Available.lua
+++ Available.lua
@@ -3,7 +3,7 @@ local Available = {
 "browser", "browsers", "calender", "cloudfile",
 "codeblock", "delete", "download", "downloadcloud",
 "gear", "upload", "uploadtocloud", "archive",
-"checkmark", "copy", "refresh", "user", "date", "version", "Egg", "Dog", "Tree", "Money"
+"checkmark", "copy", "refresh", "user", "date", "version", "Egg", "Dog", "Tree", "Money", "weight", "coin", "crown"
 }
  
 return Available
